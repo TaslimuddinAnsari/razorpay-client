8d6209c4db18d42e8ab7e12e2a99f3e339ff5ba6
TaslimuddinAnsari <taslimuddin.ansari@madgicaltechdom.com>
2023-09-21 17:45:16 +0530

    Update the UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+  const displayRazorpay = () => {
+    console.log('hello');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header className="App-header">
+        <p>Buy now!</p>
+        <button className="btn" onClick={displayRazorpay}>
+            Pay ₹500
+        </button>
+    </header>
+</div>
+  )
 }
 
-export default App;
+export default App
